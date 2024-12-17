875040cd595405493be202776b73e078798415ed
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2024-12-17 20:15:45 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Life without inspiration</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
